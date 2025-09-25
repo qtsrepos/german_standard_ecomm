8f14152237124015bec9de815f7ae1f4827642a6
muhammed-rizfan <m.rizfan@gmail.com>
2025-09-25 13:36:52 +0530

    solved the login issue

--- src/components/productItem/page.tsx
+++ src/components/productItem/page.tsx
@@ -460,7 +460,7 @@ function ProductItem(props: ProductItemProps) {
         );
         if (result.success) {
           const updatedCart = await cartServiceHelpers.getCart();
-          dispatch(setLocalCart(updatedCart));
+          dispatch(LocalCartSlice.actions.setLocalCart(updatedCart));
         }
       } else if (newQuantity <= availableQuantity) {
         // Update quantity
@@ -472,7 +472,7 @@ function ProductItem(props: ProductItemProps) {
         );
         if (result.success) {
           const updatedCart = await cartServiceHelpers.getCart();
-          dispatch(setLocalCart(updatedCart));
+          dispatch(LocalCartSlice.actions.setLocalCart(updatedCart));
         }
       } else {
         notification.warning({
@@ -581,7 +581,7 @@ function ProductItem(props: ProductItemProps) {
       if (result.success) {
         // Refresh cart state
         const updatedCart = await cartServiceHelpers.getCart();
-        dispatch(setLocalCart(updatedCart));
+        dispatch(LocalCartSlice.actions.setLocalCart(updatedCart));
       }
 
     } catch (error: any) {
